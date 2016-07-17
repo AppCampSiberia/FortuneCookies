62b01300637859380684cbe9ccc0498f1e0e3776
SashaTf <sashulya.tarantsova@gmail.com>
2016-07-17 14:50:02 +0600

    исправили ошибки

--- app/src/main/java/appcampsiberia/fortunecookies/MainActivity.java
+++ app/src/main/java/appcampsiberia/fortunecookies/MainActivity.java
@@ -1,5 +1,6 @@
 package appcampsiberia.fortunecookies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonBegin=findViewById(R.id.buttonBegin);
+        buttonBegin = (Button) findViewById(R.id.buttonBegin);
         buttonBegin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(this, FlavorActivity.class);
+                Intent intent = new Intent(MainActivity.this, FlavorActivity.class);
                 startActivity(intent);
             }
         });
