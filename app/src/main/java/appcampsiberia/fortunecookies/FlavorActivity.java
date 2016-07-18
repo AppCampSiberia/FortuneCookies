8aacd83770470bd6e68f2d40aceba2c42ffa7475
Andrey Tarantsov <andrey@tarantsov.com>
2016-07-18 14:15:14 +0600

    Сделала переход с одного слайда на другой,исправила ошибки

--- app/src/main/java/appcampsiberia/fortunecookies/FlavorActivity.java
+++ app/src/main/java/appcampsiberia/fortunecookies/FlavorActivity.java
@@ -1,13 +1,31 @@
 package appcampsiberia.fortunecookies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FlavorActivity extends AppCompatActivity {
+
+    Button buttonNext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flavor);
+
+
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlavorActivity.this, CookieActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
