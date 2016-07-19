56f4867d3bc6cfd5e1b601e19b420dca3a9dd3c2
Andrey Tarantsov <andrey@tarantsov.com>
2016-07-19 13:28:24 +0600

    изменено название

--- app/src/main/java/appcampsiberia/fortunecookies/FortuneActivity.java
+++ app/src/main/java/appcampsiberia/fortunecookies/FortuneActivity.java
@@ -5,31 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FortuneActivity extends AppCompatActivity {
 
     Button buttonMenu;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fortune);
 
-        buttonMenu = (Button)
+        buttonMenu = (Button) findViewById(R.id.buttonMenu);
 
-                findViewById(R.id.buttonMenu);
-
-        buttonMenu.setOnClickListener(new View.OnClickListener()
-
-        {
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FortuneActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-
-
         });
+        textView=  (TextView) findViewById(R.id.textViewFortune);
+        textView.setText("вы потеряете обе ноги");
     }
 }
 
