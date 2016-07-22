6fb6e27f694290819b63935b8d95b926ffee0d18
Andrey Tarantsov <andrey@tarantsov.com>
2016-07-22 15:12:57 +0600

    Добавлена кнопка about, добавлена скобка

--- app/src/main/java/appcampsiberia/fortunecookies/MainActivity.java
+++ app/src/main/java/appcampsiberia/fortunecookies/MainActivity.java
@@ -1,20 +1,20 @@
 package appcampsiberia.fortunecookies;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonBegin;
+    Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         buttonBegin = (Button) findViewById(R.id.buttonBegin);
         buttonBegin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +23,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-}
+        about = (Button) findViewById(R.id.button2);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActvity.class);
+                startActivity(intent);
+            }
+        });
+    }        
